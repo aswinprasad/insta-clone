1574bd8bcc0f0f3dd6b223b527951d4bdfd956b6
aswinprasad99 <aswinprasad99@gmail.com>
2023-01-19 00:21:40 +0530

    icon+css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Profile from "./components/Profile.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Profile />
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,71 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { Button } from "@chakra-ui/react";
 import "./Profile.css";
 
 export default function Profile() {
-
-  var picLink = "https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
-  const [pic, setPic] = useState([])
-  const [posts, setPosts] = useState([])
-  const [user, setUser] = useState({})
-
-
+  var picLink = "https://cdn-icons-png.flaticon.com/128/3177/3177440.png";
+  const [pic, setPic] = useState([]);
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     fetch(`http://jsonplaceholder.typicode.com/users/1`)
-    .then(res => res.json())
-    .then((result) => {
-      // console.log(result)
-      // setPic(result.post)
-      setUser(result)
-      // console.log(user)
-      // console.log(pic)
-    })
+      .then((res) => res.json())
+      .then((result) => {
+        setUser(result);
+      });
 
     fetch(`http://jsonplaceholder.typicode.com/photos/?_limit=100`)
-    .then(res => res.json())
-    .then((result) => {
-      // console.log(result)
-      // setPic(result.post)
-      setPic(result)
-      console.log(pic)
-    })
-  }, [])
+      .then((res) => res.json())
+      .then((result) => {
+        setPic(result);
+        console.log(pic);
+      });
+  }, []);
 
-  
   return (
     <div className="Profile">
-
       {/* profil ram,la */}
       <div className="profile-ramka">
         {/* profile img */}
         <div className="profile-image">
-        <img
-        src={user.Photo? user.Photo : picLink}
-        alt="" />
-        </div>  
-
-       {/*  profile data*/}
-       <div className="profile-data">
-        <h1>{user.name}</h1>
-        {/* profile info */}
-        <div className="pro-info">
-          <p>1501 posts</p>
-          <p>232M followers</p>
-          <p>262 following</p>
+          <img src={user.Photo ? user.Photo : picLink} alt="" />
         </div>
-       </div>
-      </div> 
 
-      <hr className="hr"/>
+        {/*  profile data*/}
+        <div className="profile-data">
+          <h1>{user.name}</h1>
+          <Button colorScheme="blue" size="sm">
+            Follow
+          </Button>
+          <Button colorScheme="gray" size="sm">
+            Message
+          </Button>
+          {/* profile info */}
+          <div className="pro-info">
+            <p>
+              <b>1501</b> posts
+            </p>
+            <p>
+              <b>232M</b> followers
+            </p>
+            <p>
+              <b>262</b> following
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <hr className="hr" />
       {/* profile galary */}
       <div className="gallary">
-        {pic.map((pics)=>{
-          return <img key={pics.id} src={ pics.url }
-          
-           className="item"></img>
+        {pic.map((pics) => {
+          return <img key={pics.id} src={pics.url} className="item"></img>;
         })}
       </div>
-      
     </div>
-  )
+  );
 }
